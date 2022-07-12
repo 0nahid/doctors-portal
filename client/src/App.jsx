cc07f9e4094d3f3cd3667de9bd72d17dfac4cefc
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-07-12 16:23:57 +0600

    added preloader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { Toaster } from 'react-hot-toast'
 import { Route, Routes } from 'react-router-dom'
 import Appointment from './Components/Appointment/Appointment'
@@ -5,25 +6,40 @@ import Login from './Components/Authentication/Login'
 import RequiredAuth from './Components/Authentication/RequiredAuth'
 import SignUp from './Components/Authentication/SignUp'
 import Home from './Components/Home/Home'
+import Loader from './Components/Shared/Loader/Loader'
 import Navbar from './Components/Shared/Navbar/Navbar'
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <div className="max-w-7xl mx-auto">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/appointment" element={
-          <RequiredAuth>
-            <Appointment />
-          </RequiredAuth>
-        } />
+    <div>
+      <div className="max-w-7xl mx-auto">
+        {
+          loading ? (
+            <Loader />
+          ) :
+            <Navbar />
+        }
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/appointment" element={
+            <RequiredAuth>
+              <Appointment />
+            </RequiredAuth>
+          } />
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-      <Toaster />
-
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+        <Toaster />
+      </div>
     </div>
   )
 }
